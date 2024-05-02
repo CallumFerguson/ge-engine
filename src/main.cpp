3f067cba7f332d3352e671dc7db73be6e3dda209
Callum Ferguson <Calxferg@gmail.com>
2024-05-02 01:25:01 -0700

    add glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+#include <glm/mat4x4.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/scalar_constants.hpp>
+
 int main() {
     std::cout << "test" << std::endl;
+
+    glm::mat4 Projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
+    std::cout << Projection[0][0] << std::endl;
+
+    std::cout << glm::pi<float>() << std::endl;
 }
