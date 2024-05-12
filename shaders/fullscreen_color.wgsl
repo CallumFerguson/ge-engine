bc4bf2995a63526a4621afa6cff9490400d70e7e
Callum Ferguson <Calxferg@gmail.com>
2024-05-12 02:21:39 -0700

    basic render pass

---
+++ shaders/fullscreen_color.wgsl
@@ -0,0 +1,27 @@
+struct VertexInput {
+    @builtin(vertex_index) vertexIndex: u32,
+}
+
+struct VertexOutput {
+    @builtin(position) position: vec4f,
+}
+
+@vertex
+fn vert(i: VertexInput) -> VertexOutput {
+    let positions = array<vec2f, 3>(
+      vec2f(-1, -1),
+      vec2f(3, -1),
+      vec2f(-1, 3),
+    );
+
+    var o: VertexOutput;
+
+    o.position = vec4(positions[i.vertexIndex], 0, 1);
+
+    return o;
+}
+
+@fragment
+fn frag(i: VertexOutput) -> @location(0) vec4f {
+    return vec4(0.25, 0, 0, 1);
+}
--- src/webgpu.cpp
+++ src/webgpu.cpp
@@ -1,40 +1,12 @@
 #include "webgpu.hpp"
 
 #include <iostream>
-#include <emscripten.h>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include <lib_webgpu.h>
-#include <assert.h>
 
 #include "gltfloader.hpp"
-
-//void ObtainedWebGpuDevice(WGpuDevice result, void *userData) {
-//    device = result;
-//
-//    if (!device) {
-//        std::cout << "could not get device" << std::endl;
-//    }
-//
-//    std::cout << "got device" << std::endl;
-//    std::cout << device << std::endl;
-//}
-//
-//void ObtainedWebGpuAdapter(WGpuAdapter result, void *userData) {
-//    adapter = result;
-//
-//    if (!adapter) {
-//        std::cout << "could not get adapter" << std::endl;
-//    }
-//
-//    std::cout << "got adapter" << std::endl;
-//    std::cout << adapter << std::endl;
-//
-//    WGpuDeviceDescriptor deviceDesc = {};
-//
-//    WGpuSupportedLimits requiredLimits = {};
-//
-//    deviceDesc.requiredLimits = requiredLimits;
-//    wgpu_adapter_request_device_async(adapter, &deviceDesc, ObtainedWebGpuDevice, nullptr);
-//}
 
 void webgpuTest() {
     std::cout << "webgpu test start" << std::endl;
@@ -54,7 +26,7 @@ void webgpuTest() {
         throw std::runtime_error("could not get adapter");
     }
 
-    auto features = wgpu_adapter_or_device_get_features(adapter);
+    auto features = wgpu_adapter_get_features(adapter);
 
     int requiredFeatures = 0;
 
@@ -85,4 +57,63 @@ void webgpuTest() {
     config.device = device;
     config.format = presentationFormat;
     wgpu_canvas_context_configure(canvasContext, &config);
+
+    auto colorAttachment = WGPU_RENDER_PASS_COLOR_ATTACHMENT_DEFAULT_INITIALIZER;
+    WGpuColor clearColor{0, 0, 0, 1};
+    colorAttachment.clearValue = clearColor;
+    colorAttachment.loadOp = WGPU_LOAD_OP_CLEAR;
+    colorAttachment.storeOp = WGPU_STORE_OP_STORE;
+    colorAttachment.view = wgpu_texture_create_view(wgpu_canvas_context_get_current_texture(canvasContext));
+
+    auto renderPassDescriptor = WGPU_RENDER_PASS_DESCRIPTOR_DEFAULT_INITIALIZER;
+    renderPassDescriptor.numColorAttachments = 1;
+    renderPassDescriptor.colorAttachments = &colorAttachment;
+
+    auto commandEncoder = wgpu_device_create_command_encoder_simple(device);
+
+    auto renderPassEncoder = wgpu_command_encoder_begin_render_pass(commandEncoder, &renderPassDescriptor);
+
+    std::ifstream shaderFile("shaders/fullscreen_color.wgsl", std::ios::binary);
+    if(!shaderFile) {
+        throw std::runtime_error("Could not open shader file");
+    }
+    std::stringstream shaderBuffer;
+    shaderBuffer << shaderFile.rdbuf();
+
+    WGpuShaderModuleDescriptor shaderModuleDescriptor = {};
+    shaderModuleDescriptor.code = shaderBuffer.str().c_str();
+    auto shaderModule = wgpu_device_create_shader_module(device, &shaderModuleDescriptor);
+
+    auto pipelineLayout = wgpu_device_create_pipeline_layout(device, nullptr, 0);
+
+    auto pipelineDescriptor = WGPU_RENDER_PIPELINE_DESCRIPTOR_DEFAULT_INITIALIZER;
+    pipelineDescriptor.layout = pipelineLayout;
+
+    pipelineDescriptor.vertex.module = shaderModule;
+    pipelineDescriptor.vertex.entryPoint = "vert";
+    pipelineDescriptor.vertex.numBuffers = 0;
+
+    auto target = WGPU_COLOR_TARGET_STATE_DEFAULT_INITIALIZER;
+    target.format = presentationFormat;
+
+    pipelineDescriptor.fragment.module = shaderModule;
+    pipelineDescriptor.fragment.entryPoint = "frag";
+    pipelineDescriptor.fragment.numTargets = 1;
+    pipelineDescriptor.fragment.targets = &target;
+
+    pipelineDescriptor.primitive.topology = WGPU_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    pipelineDescriptor.primitive.cullMode = WGPU_CULL_MODE_BACK;
+
+    pipelineDescriptor.multisample.count = 1;
+
+    auto pipeline = wgpu_device_create_render_pipeline(device, &pipelineDescriptor);
+
+    wgpu_render_pass_encoder_set_pipeline(renderPassEncoder, pipeline);
+    wgpu_render_pass_encoder_draw(renderPassEncoder, 3, 1, 0, 0);
+
+    wgpu_render_pass_encoder_end(renderPassEncoder);
+
+    auto queue = wgpu_device_get_queue(device);
+    wgpu_queue_submit_one_and_destroy(queue, wgpu_command_encoder_finish(commandEncoder));
+
 }
