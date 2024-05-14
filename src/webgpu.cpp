ef89c6656fa7a1fd13a6a8d3947bb431f5a1178d
Callum Ferguson <Calxferg@gmail.com>
2024-05-13 17:09:14 -0700

    fix js formatting

--- src/webgpu.cpp
+++ src/webgpu.cpp
@@ -23,35 +23,35 @@ void resizeCanvas() {
 
     // @formatter:off
     EM_ASM({
-               const canvas = Module.canvas;
-               const width = Math.max(1, Math.min($0, canvas.clientWidth));
-               const height = Math.max(1, Math.min($0, canvas.clientHeight));
+           const canvas = Module.canvas;
+           const width = Math.max(1, Math.min($0, canvas.clientWidth));
+           const height = Math.max(1, Math.min($0, canvas.clientHeight));
 
-               const needResize = width !== canvas.width || height !== canvas.height;
-               if (needResize) {
-                   canvas.width = width;
-                   canvas.height = height;
-               }
-           }, supportedLimits.limits.maxTextureDimension2D);
+           const needResize = width !== canvas.width || height !== canvas.height;
+           if (needResize) {
+               canvas.width = width;
+               canvas.height = height;
+           }
+       }, supportedLimits.limits.maxTextureDimension2D);
     // @formatter:on
 }
 
 // @formatter:off
 uint32_t getCanvasWidth() {
     return EM_ASM_INT(
-            if (Module.canvas) {
-                return canvas.width;
-            }
-            return 0;
+        if (Module.canvas) {
+            return canvas.width;
+        }
+        return 0;
     );
 }
 
 uint32_t getCanvasHeight() {
     return EM_ASM_INT(
-            if (Module.canvas) {
-                return canvas.height;
-            }
-            return 0;
+        if (Module.canvas) {
+            return canvas.height;
+        }
+        return 0;
     );
 }
 // @formatter:on
