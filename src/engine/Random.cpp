0f681b2a99826df0f1f7f8c75c437ba274834c8e
Callum Ferguson <Calxferg@gmail.com>
2024-05-28 02:03:18 -0700

    static random class

---
+++ src/engine/Random.cpp
@@ -0,0 +1,11 @@
+#include "Random.hpp"
+
+#include <random>
+
+static std::random_device rd;
+static std::mt19937 mt(rd());
+static std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+
+float Random::value() {
+    return dist(mt);
+}
