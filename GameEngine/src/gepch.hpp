e72744440bfb4aa06e361014511fe3aae68cee65
Callum Ferguson <Calxferg@gmail.com>
2024-05-30 23:51:51 -0700

    precompiled header

---
+++ GameEngine/src/gepch.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <optional>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <array>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <entt/entt.hpp>
+#include <tiny_gltf.h>
+#include <GLFW/glfw3.h>
+#include <webgpu/webgpu_cpp.h>
+#include <glm/glm.hpp>
