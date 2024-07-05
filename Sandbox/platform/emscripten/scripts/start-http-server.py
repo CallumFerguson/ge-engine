b2c4420a8c4bf92f87ddc87f2a01df6765c9df4a
Callum Ferguson <Calxferg@gmail.com>
2024-07-05 15:01:05 -0700

    http run uuid with hash instead of localStorage

--- Sandbox/platform/emscripten/scripts/start-http-server.py
+++ Sandbox/platform/emscripten/scripts/start-http-server.py
@@ -4,20 +4,20 @@ import uuid
 import os
 import sys
 
+run_uuid = str(uuid.uuid4())
+
 
 class NoCacheHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    run_hash = str(uuid.uuid4())
-
     def __init__(self, *args, **kwargs):
         kwargs['directory'] = directory
         super().__init__(*args, **kwargs)
 
     def do_GET(self):
-        if self.path == '/runhash':
+        if self.path == '/run_uuid':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(self.run_hash.encode())
+            self.wfile.write(run_uuid.encode())
         else:
             super().do_GET()
 
@@ -36,10 +36,10 @@ if __name__ == "__main__":
         print("Usage: python start-http-server.py <directory>")
         sys.exit(1)
 
-    directory = os.path.join(os.path.abspath(sys.argv[1]), "dist")
+    directory = os.path.abspath(sys.argv[1])
 
     PORT = 3002
     handler = NoCacheHTTPRequestHandler
     with socketserver.TCPServer(("", PORT), handler) as httpd:
-        print("\nserver started at http://127.0.0.1:3002")
+        print("\nserver started at http://127.0.0.1:3002/#{}".format(run_uuid))
         httpd.serve_forever()
